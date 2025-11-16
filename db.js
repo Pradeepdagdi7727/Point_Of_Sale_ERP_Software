deeef36fd164da1d97ca709e116adcf52decb6fe
pradeep dagdi <dagdipradeep@gmail.com>
2025-11-16 20:46:25 +0530

    update db connection

--- db.js
+++ db.js
@@ -1,18 +1,20 @@
-const mysql  = require("mysql");
+const mysql = require("mysql");
 
 const db = mysql.createConnection({
-    host: process.env.DB_HOST || 'localhost',
-    user: process.env.DB_USER || 'root',
-    password: process.env.DB_PASS || '',
-    database: process.env.DB_NAME || 'supermarketDB'
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
+    connectTimeout: 20000
 });
 
 db.connect(err => {
     if (err) {
         console.error('DB connection error:', err);
-        process.exit(1);
+        return;
     }
-    console.log("database connection successfull");
+    console.log("Connected to Railway MySQL!");
 });
 
 module.exports = db;
