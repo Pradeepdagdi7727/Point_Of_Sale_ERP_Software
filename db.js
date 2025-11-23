af29059bfb1ccfe5266e243d0576a9972cf750d4
pradeep dagdi <dagdipradeep@gmail.com>
2025-11-23 23:16:05 +0530

    chang in db

--- db.js
+++ db.js
@@ -1,22 +1,18 @@
 const mysql = require("mysql2");
 
-const db = mysql.createConnection({
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASS,
-    database: process.env.DB_NAME,
-    ssl: {
-        rejectUnauthorized: false
-    }
+const pool = mysql.createPool({
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,
+  ssl: {
+    rejectUnauthorized: false,
+  }
 });
 
-db.connect(err => {
-    if (err) {
-        console.error("DB connection error:", err);
-        return;
-    }
-    console.log("Connected to Railway MySQL!");
-});
 
-module.exports = db;
+module.exports = pool;
